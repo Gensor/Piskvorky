2fa6bc3899073a34bb86da6db03a6318c7063f24
Gensor <Gensor@users.noreply.github.com>
2020-03-22 20:13:27 +0100

    Initial commit

---
+++ app/src/main/java/com/piskvorky/MainActivity.java
@@ -0,0 +1,19 @@
+package com.piskvorky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO: obrazok pre tlacidla {X},{O}
+        //TODO: pole pre tlacidla {0 -prazdne ,-1 je {X}, 1 je {O}
+        //TODO: boolean jeVytaz metodu na overenie vysledku diagonalne,horizontalne,vertikalne ci je z toho nieco -3 alebo 3
+        //TODO: vypis kto vyhral a resetni hracie pole
+        //TODO: tlacidlo na novu hru
+    }
+}
