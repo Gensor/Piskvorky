96e2d47d95b50143d162a5ff3f0965c838f7a2d5
Gensor <Gensor@users.noreply.github.com>
2020-03-23 20:41:58 +0100

    Update MainActivity.java

--- app/src/main/java/com/piskvorky/MainActivity.java
+++ app/src/main/java/com/piskvorky/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO: vytvor classu hra
         //TODO: obrazok pre tlacidla {X},{O}
         //TODO: pole pre tlacidla {0 -prazdne ,-1 je {X}, 1 je {O}
         //TODO: boolean jeVytaz metodu na overenie vysledku diagonalne,horizontalne,vertikalne ci je z toho nieco -3 alebo 3
