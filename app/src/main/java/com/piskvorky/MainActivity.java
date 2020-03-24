ba8ba2f78778b546d4f985185a2e4a93d2828d33
Gensor <Gensor@users.noreply.github.com>
2020-03-24 19:00:05 +0100

    Update MainActivity.java

--- app/src/main/java/com/piskvorky/MainActivity.java
+++ app/src/main/java/com/piskvorky/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO: vytvor classu hra
-        //TODO: obrazok pre tlacidla {X},{O}
         //TODO: pole pre tlacidla {0 -prazdne ,-1 je {X}, 1 je {O}
         //TODO: boolean jeVytaz metodu na overenie vysledku diagonalne,horizontalne,vertikalne ci je z toho nieco -3 alebo 3
         //TODO: vypis kto vyhral a resetni hracie pole
