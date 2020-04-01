fe59f173218b7d78886b6cfa5a0bde365fe6539f
Gensor <Gensor@users.noreply.github.com>
2020-04-02 00:06:35 +0200

    1

---
+++ app/src/test/java/com/piskvorky/HraTest.groovy
@@ -0,0 +1,12 @@
+package com.piskvorky
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HraTest  {
+    @Test
+    void testMa_Bod() {
+        int jedna=1;
+        int dva=1;
+        assertEquals(jedna,dva);
+    }
+}
